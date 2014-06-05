afcf8c1068c4df98283a3eea1f76c46723a0b197
Tara Marchand <tara@mac.com>
2014-06-04 17:33:06 -0700

    Socrata API info in D3 JS

--- public/scripts/d3demo.js
+++ public/scripts/d3demo.js
@@ -20,3 +20,6 @@ bars.attr("x", function(d, i) {
     .attr("height", function(d, i) {
         return d;
     });
+
+// http://data.sfgov.org/resource/vw6y-z8j6.json?$$app_token=SOCRATA_APP_TOKEN&$where=opened > '2014-01-01 07:00:00'
+// X-App-Token SOCRATA_APP_TOKEN
