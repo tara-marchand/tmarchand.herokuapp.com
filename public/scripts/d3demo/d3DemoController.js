158cdaf884c04031d1f553c6ec6059fa477913a1
Tara Marchand <tara@mac.com>
2014-07-21 18:11:48 -0700

    Starting to use real data

--- public/scripts/d3demo/d3DemoController.js
+++ public/scripts/d3demo/d3DemoController.js
@@ -2,12 +2,4 @@ angular.module("d3Demo")
     .controller("d3DemoController", ["d3DemoService", function d3DemoController(d3DemoService) {
         "use strict";
 
-        var sf311Data = [];
-
-        function saveSf311Data(data, status, headers, config) {
-            sf311Data = data;
-            console.dir(sf311Data);
-        }
-
-        d3DemoService.getSf311Data().success(saveSf311Data);
     }]);
