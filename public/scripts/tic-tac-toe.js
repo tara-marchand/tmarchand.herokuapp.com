f73d1d7946734fa95bc98d67934b674640c6d28e
Tara Marchand <tara@mac.com>
2014-11-14 08:25:20 -0800

    More tic-tac-toe work

--- public/scripts/tic-tac-toe.js
+++ public/scripts/tic-tac-toe.js
@@ -1,21 +1,34 @@
+// model
 var TttModel = function() {
-    var model = [
-    ];
+    var model = [];
 };
 
 TttModel.prototype = {
 
 };
 
+// view
 TttView = function() {
     var model = new TttModel();
+    var board = document.getElementById("board");
+    var rows = board.getElementsByTagName("tr");
 
-    this.clear = function() {
+    // add data attributes with row and column values to spaces
+    for (var i = rows.length - 1, spaces; i >= 0; i--) {
+        spaces = rows[i].getElementsByTagName("td");
+        for (var j = spaces.length - 1; j >= 0; j--) {
+            spaces[j].setAttribute("data-row-column", "space-" + i + "-" + j);
+        }
+    }
+
+    // check if board is in a winning state
+    this.isWin = function() {
+    };
+
+    this.selectSpace = function() {
 
     };
-};
 
-TttView.prototype = {
 
 };
 
