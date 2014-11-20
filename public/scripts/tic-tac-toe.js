43295f88522b863bbcf8ee4f6891bc7e71db2211
Tara Marchand <tara@mac.com>
2014-11-19 16:37:57 -0800

    Tic-tac-toe: Started implementing minimax algorithm for computer move

--- public/scripts/tic-tac-toe.js
+++ public/scripts/tic-tac-toe.js
@@ -12,25 +12,26 @@ var Board = function() {
     var numberOfMoves = 0;
     var maxNumberOfMoves = numberOfSpacesPerSide * numberOfSpacesPerSide; // calculated on iteration of table cells
 
-    var selectSpace = function(el, content) {
+    var selectSpace = function(el, board, content) {
         el.textContent = content;
     }.bind(this);
 
-    var getContentByColumnAndRow = function(column, row) {
-        return boardEl.querySelector("[data-column=\"" + column + "\"][data-row=\"" + row + "\"]").textContent;
+    var getContentByColumnAndRow = function(board, column, row) {
+        return board.querySelector("[data-column=\"" + column + "\"][data-row=\"" + row + "\"]").textContent;
     };
 
     // http://stackoverflow.com/questions/1056316/algorithm-for-determining-tic-tac-toe-game-over/1056352#1056352
     // determine whether there's a win, and if so, who won
-    var checkForWin = function(targetColumn, targetRow, content) {
+    var checkForWin = function(board, targetColumn, targetRow, content) {
         var isWin = {
             boolean: false,
-            content: content
+            content: content,
+            score: null
         };
 
         // check column
         for (var i = 0; i < numberOfSpacesPerSide; i++) {
-            if (getContentByColumnAndRow(targetColumn, i) !== content) {
+            if (getContentByColumnAndRow(board, targetColumn, i) !== content) {
                 break;
             }
             if (i === numberOfSpacesPerSide - 1) {
@@ -39,7 +40,7 @@ var Board = function() {
         }
         // check row
         for (i = 0; i < numberOfSpacesPerSide; i++){
-            if (getContentByColumnAndRow(i, targetRow) !== content) {
+            if (getContentByColumnAndRow(board, i, targetRow) !== content) {
                 break;
             }
             if (i === numberOfSpacesPerSide - 1) {
@@ -49,7 +50,7 @@ var Board = function() {
         // check diagonal
         if (targetColumn === targetRow) {
             for (i = 0; i < numberOfSpacesPerSide; i++) {
-                if (getContentByColumnAndRow(i, i) !== content) {
+                if (getContentByColumnAndRow(board, i, i) !== content) {
                     break;
                 }
                 if (i === numberOfSpacesPerSide - 1) {
@@ -59,7 +60,7 @@ var Board = function() {
         }
         // check opposite diagonal
         for (i = 0; i < numberOfSpacesPerSide; i++) { // 20, 11, 02
-            if (getContentByColumnAndRow(numberOfSpacesPerSide - 1 - i, i) !== content) {
+            if (getContentByColumnAndRow(board, numberOfSpacesPerSide - 1 - i, i) !== content) {
                 break;
             }
             if (i === numberOfSpacesPerSide - 1) {
@@ -67,10 +68,18 @@ var Board = function() {
             }
         }
 
+        if (isWin.boolean === true) {
+            if (content === USER_TEXT_CONTENT) {
+                isWin.score = 10;
+            } else if (content === COMPUTER_TEXT_CONTENT) {
+                isWin.score = -10;
+            }
+        }
+
         return isWin;
     };
 
-    var doUserMove = function(e) {
+    var doUserMove = function(e, board) {
         if (numberOfMoves === maxNumberOfMoves) {
             return;
         }
@@ -83,11 +92,12 @@ var Board = function() {
             var targetRow = target.getAttribute(ROW_DATA_ATTRIBUTE);
             var isWin = {};
 
-            selectSpace(target, USER_TEXT_CONTENT);
+            selectSpace(target, board, USER_TEXT_CONTENT);
             // user won
-            isWin = checkForWin(targetColumn, targetRow, USER_TEXT_CONTENT);
+            isWin = checkForWin(boardEl, targetColumn, targetRow, USER_TEXT_CONTENT);
             if (isWin.boolean === true) {
-                alert("Win for " + isWin.content + "!");
+                var name = (isWin.score === 10) ? "user" : "computer";
+                alert("Win for " + name + "!");
                 return;
             }
             // user didn't win
@@ -96,12 +106,66 @@ var Board = function() {
         }
     };
 
+    var getAvailableMoves = function(board) {
+        var availableMoves = [];
+
+        for (var i = numberOfSpacesPerSide - 1, rowSpaceEls; i >= 0; i--) {
+            rowSpaceEls = rowEls[i].getElementsByTagName("td");
+            for (var j = rowSpaceEls.length - 1; j >= 0; j--) {
+                if (rowSpaceEls[j].textContent === "") {
+                    availableMoves.push({ column: rowSpaceEls[j].getAttribute(COLUMN_DATA_ATTRIBUTE), row: rowSpaceEls[j].getAttribute(ROW_DATA_ATTRIBUTE) });
+                }
+            }
+        }
+
+        return availableMoves;
+    };
+
     var doComputerMove = function() {
         if (numberOfMoves === maxNumberOfMoves) {
             return;
         }
 
-        // determine which space to select, then select it
+        minimax(boardEl, 0);
+
+        isWin = checkForWin(boardEl, targetColumn, targetRow, USER_TEXT_CONTENT);
+        if (isWin.boolean === true) {
+            var name = (isWin.score === 10) ? "user" : "computer";
+            alert("Win for " + name + "!");
+            return;
+        }
+        // computer didn't win
+        numberOfMoves++;
+    };
+
+    var minimax = function(board, depth) {
+        var availableMoves = getAvailableMoves(board);
+        var scores = []; // an array of scores
+        var moves = []; // an array of moves
+        var minScoreIndex;
+        var moveToMake = {};
+
+        // populate the scores array, recursing as needed
+        for (var i = availableMoves.length - 1, move, boardCopy, selector, target; i >= 0; i--) {
+            move = availableMoves[i];
+            boardCopy = board.cloneNode();
+            selector = "td[" + COLUMN_DATA_ATTRIBUTE + "=\"" + move.column + "\"][" + ROW_DATA_ATTRIBUTE + "=\"" + move.row + "\"]";
+            console.log(selector);
+            target = board.querySelector(selector);
+            selectSpace(target, boardCopy, COMPUTER_TEXT_CONTENT);
+        }
+
+        // game.get_available_moves.each do |move|
+        //     possible_game = game.get_new_state(move)
+        //     scores.push minimax(possible_game)
+        //     moves.push move
+        // end
+
+        // # Do the min or the max calculation
+        // # This is the min calculation
+        minScoreIndex = scores.indexOf(Math.min.apply(null, scores));
+        moveToMake = moves[minScoreIndex];
+        // return scores[min_score_index]
     };
 
     // add data attributes with row and column values to spaces
@@ -116,7 +180,9 @@ var Board = function() {
     }
 
     // event delegation
-    boardEl.addEventListener("click", doUserMove);
+    boardEl.addEventListener("click", function(e) {
+        doUserMove(e, boardEl);
+    });
 };
 
 var board = new Board();
