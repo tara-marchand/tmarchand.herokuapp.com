368d8e43934ec867a7dad3eb2a86b20069a99a08
Tara Marchand <tara@mac.com>
2014-12-31 13:59:20 -0800

    Further along in playing with Ember

---
+++ public/scripts/ember/controllers/todos-controller.js
@@ -0,0 +1,32 @@
+Todos.TodosController = Ember.ArrayController.extend({
+    actions: {
+        createTodo: function() {
+            // get the todo title set by the "New Todo" text field
+            var title = this.get("newTitle");
+            if (!title.trim()) {
+                return;
+            }
+
+            // create the new Todo model
+            var todo = this.store.createRecord("todo", {
+                title: title,
+                isCompleted: false
+            });
+
+            // clear the "New Todo" text field
+            this.set("newTitle", "");
+
+            // save the new model
+            todo.save();
+        }
+    },
+
+    remaining: function() {
+        return this.filterBy("isCompleted", false).get("length");
+    }.property("@each.isCompleted"),
+
+    inflection: function() {
+        var remaining = this.get("remaining");
+        return remaining === 1 ? "item" : "items";
+    }.property("remaining")
+});
