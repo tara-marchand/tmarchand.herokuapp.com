304efa56b9a37710c36480d549a5ca7107815145
Tara Marchand <tara@mac.com>
2015-06-07 12:33:17 -0700

    Renamed photos files

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var gulp = require('gulp');
 
 /* Instagram photos w/React START */
 
-gulp.task('photos-bundle', function() {
+gulp.task('photos-lib', function() {
     'use strict';
 
     var browserifyBundle = browserify();
@@ -17,23 +17,23 @@ gulp.task('photos-bundle', function() {
     browserifyBundle.require('react');
     browserifyBundle.require('react-async');
     browserifyBundle.bundle()
-        .pipe(source('photos-bundle.js'))
+        .pipe(source('photos-lib.js'))
         .pipe(gulp.dest('./public/scripts/photos'));
 });
 
-gulp.task('photos-react-server', function() {
+gulp.task('photos-server', function() {
     'use strict';
 
-    return gulp.src('./views/jsx/photos.jsx')
+    return gulp.src('./public/scripts/photos/photos.jsx')
         .pipe(react())
-        .pipe(rename('react-server.js'))
+        .pipe(rename('photos-server.js'))
         .pipe(gulp.dest('./public/scripts/photos'));
 });
 
-gulp.task('photos-react-browser', function() {
+gulp.task('photos-browser', function() {
     'use strict';
 
-    var reactFiles = glob.sync('./views/jsx/photos.jsx');
+    var reactFiles = glob.sync('./public/scripts/photos/photos.jsx');
     var bundler = browserify({
             entries: reactFiles,
             transform: ['reactify'],
@@ -45,11 +45,11 @@ gulp.task('photos-react-browser', function() {
     var watcher = watchify(bundler);
     return watcher.on('update', function() {
         watcher.bundle()
-        .pipe(source('react-browser.js'))
+        .pipe(source('photos-browser.js'))
         .pipe(gulp.dest('./public/scripts/photos'));
     })
     .bundle()
-    .pipe(source('react-browser.js'))
+    .pipe(source('photos-browser.js'))
     .pipe(gulp.dest('./public/scripts/photos'));
 });
 
@@ -77,7 +77,7 @@ gulp.task('photos-react-browser', function() {
 
 // gulp.task('contractors', ['lib', 'app']);
 
-gulp.task('watch', ['photos-react-server', 'photos-browserify-bundle', 'photos-react-browser'], function() {
+gulp.task('watch', ['photos-lib', 'photos-server', 'photos-browser'], function() {
     'use strict';
 
     gulp.watch('views/jsx/**/*.jsx', ['photos-react-server', 'photos-react-browser']);
